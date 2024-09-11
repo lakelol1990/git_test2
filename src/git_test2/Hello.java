227989641aad68a2e00218f58201f29699e1a69f
Choi, MinJi <mamber0243@naver.com>
2024-09-11 09:52:55 +0900

    Hello 클래스에 message 멤버 변수 추가

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,9 +1,11 @@
 package git_test2;
 
 public class Hello {
+
+	String message ="안녕하세요";
 	
-	public void say() {
-		System.out.println("hello world");
-	}
+public void say() {
+System.out.println(message);
+}
 
 }
